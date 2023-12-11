f15f4e73e87db3ec5ab1af6b1536f40011b6b3cf
Regine Wang <LinLing0091@hotmail.com>
2023-12-10 19:08:59 -0500

    Version 1 Resume.cs

---
+++ Final_Project/Resume.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    public class Resume
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public String Gender { get; set; }
+        public int Age { get; set; }
+        public string ContactInfo { get; set; }
+        public string Experience { get; set; }
+        public string Education { get; set; }
+        public string Hobbies { get; set; }
+        public string References { get; set; }
+
+
+        public override string ToString()
+        {
+            string formatted = String.Format("{0}\t {1}\t {2} \t, {3}\t, {4} year-old\nContact Information: {5}\nWork Experience: {6}\nEducation: {7}\nHobbies: {8}\nReferences: {9}", 
+                                              Id, FirstName, LastName, Gender, Age, ContactInfo, Experience, Education, Hobbies, References);
+            return formatted;
+        }
+    }
+}
